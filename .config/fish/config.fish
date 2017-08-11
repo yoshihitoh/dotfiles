37562d68003a0b93ae380b4e6cab7b44c2d7bf44
Yoshihito Hirakawa <hammer.and.heart.daphne@gmail.com>
2017-08-11 17:53:26 +0900

    Add fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,44 @@
+# set fish_theme agnoster
+
+# rust
+#------------------------------------------------------------------------------
+set -x PATH $HOME/bin $HOME/.cargo/bin $PATH
+set -x RUST_SRC_PATH $HOME/workspace/tools/lang/rust/rustc-1.19.0-src/src
+
+# python
+#------------------------------------------------------------------------------
+set -x PATH $HOME/.pyenv/shims $PATH
+
+# rbenv
+#------------------------------------------------------------------------------
+rbenv init - | source
+
+# pyenv
+#------------------------------------------------------------------------------
+. (pyenv init - | psub)
+
+# peco
+#------------------------------------------------------------------------------
+function fish_user_key_bindings
+    bind \cr peco_select_history
+end
+
+# android sdk
+#------------------------------------------------------------------------------
+set ANDROID_SDK $HOME/Library/Android/sdk
+set -x PATH $ANDROID_SDK/tools $ANDROID_SDK/platform-tools $PATH
+
+# emsdk
+#------------------------------------------------------------------------------
+set -x EMSDK $HOME/workspace/tools/emscripten/emsdk-portable
+set -x EMSCRIPTEN = $EMSDK/emscripten/1.37.16
+set -x PATH $EMSDK $PATH
+set -x PATH $EMSCRIPTEN $PATH
+set -x EM_CONFIG $HOME/.emscripten
+set -x BINARYEN_ROOT = $EMSDK/clang/e1.37.16_64bit/binaryen
+
+# clang/nodejsはEM_CONFIGに設定済み
+# emsdk_env.sh だと両者をPATHに追加しているが、
+# PATHに設定すると通常のclang/nodejsと干渉するので除外する
+# set -x PATH $EMSDK/clang/e1.37.16_64bit $PATH
+# set -x PATH $EMSDK/node/4.1.1_64bit/bin $PATH
