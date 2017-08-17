2f777a77e9eb768a7a8a6496efe9aff1737d9e50
Yoshihito Hirakawa <hammer.and.heart.daphne@gmail.com>
2017-08-17 21:41:52 +0900

    set fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
-# set fish_theme agnoster
+
+# theme
+#------------------------------------------------------------------------------
+set -g fish_theme agnoster
+# set -g fish_theme bobthefish
+# set -g theme_color_scheme solarized-light
 
 # rust
 #------------------------------------------------------------------------------
